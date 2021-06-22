5de7d8d12eb3ca1771f1de43b39916fa71bb46ae
Duncan <duncan@Duncans-iMac.local>
2021-06-22 01:45:09 -0700

    Created Getting started project

---
+++ Getting Started/Getting Started/Getting Started/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Getting Started
+//
+//  Created by Duncan on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+
+}
+
+
