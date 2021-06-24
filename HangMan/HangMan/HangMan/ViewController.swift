4f6a5440efa6bccbcaf83a80abd2b448d3709ab9
DSkilton <the_skiltons@hotmail.com>
2021-06-24 11:30:51 -0700

    udpdate

--- HangMan/HangMan/HangMan/ViewController.swift
+++ HangMan/HangMan/HangMan/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWorldLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
 
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton){
+        sender.isEnabled = false
+    }
+	    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
 }
 
