562efe321f2eb13a3a7d21be42ee7d2f5dc8f4a3
DSkilton <the_skiltons@hotmail.com>
2021-06-24 02:00:26 -0700

    started traffic lights seques

---
+++ Traffic Lights/Traffic Lights/Traffic Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Lights
+//
+//  Created by Duncan on 24/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
